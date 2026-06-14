30845a20a91664866758e458be4b5ddd28f11b26
Hà Thúc Khương Ninh <hathuckhuongninh@gmail.com>
2026-06-14 13:15:00 +0700

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,41 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Enrollment, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
-
+# Register your models here.
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+# --- CẤU HÌNH INLINE CHO QUESTION VÀ CHOICE THEO YÊU CẦU QUESTION 2 ---
 
-# Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('content', 'grade')
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'order']
 
-
-# <HINT> Register Question and Choice models here
-
+# Đăng ký các Model vào hệ thống Admin Site
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Enrollment)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
